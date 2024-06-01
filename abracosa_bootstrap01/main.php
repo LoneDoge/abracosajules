256e03b2d87ef035a9672bbf0c3767507b0310e1
LoneDoge <121995510+LoneDoge@users.noreply.github.com>
2024-06-01 09:20:23 +0900

    Add files via upload

---
+++ abracosa_bootstrap01/main.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instagram</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel = "stylesheet" href = "styles.css">
+</head>
+<body>
+    <div class = "container fluid mt-4">
+        <div class = "row">
+            <div class = "col-sm-2">
+                <img src="insta-white-bare.png" class="img-fluid width: 30px;" style = "width:100px; position: fixed;" alt="instaLogo">
+                <div class = "container">
+                    <table class="table mt-4 table-hover table-borderless">
+                        <tbody>
+                        <tr class = "text-white" style = "position: fixed; top: 78px">
+                            <td><img src="home-icon.png" class="img-fluid" id="icon" alt="home"></td><td>Home</td>
+                        </tr>
+                        <tr class = "text-white" style = "position: fixed; top: 118px">
+                            <td><img src="search-icon.png" class="img-fluid" id="icon" alt="search"></td><td>Search</td>
+                        </tr>
+                        <tr class = "text-white" style = "position: fixed; top: 158px">
+                            <td><img src="explore-icon.png" class="img-fluid" id="icon" alt="explore"></td><td>Explore</td>
+                        </tr>
+                        <tr class = "text-white" style = "position: fixed; top: 198px">
+                            <td><img src="reels-icon.png" class="img-fluid" id="icon" alt="reels"></td><td>Reels</td>
+                        </tr>
+                        <tr class = "text-white" style = "position: fixed; top: 238px">
+                            <td><img src="mess-icon.png" class="img-fluid" id="icon" alt="mess"></td><td>Messages</td>
+                        </tr>
+                        <tr class = "text-white" style = "position: fixed; top: 278px">
+                            <td><img src="notif-icon.png" class="img-fluid" id="icon" alt="notif"></td><td>Notifications</td>
+                        </tr>
+                        <tr class = "text-white" style = "position: fixed; top: 318px">
+                            <td><img src="create-icon.png" class="img-fluid" id="icon" alt="create"></td><td>Create</td>
+                        </tr>
+                        <tr class = "text-white" style = "position: fixed; top: 358px">
+                            <td><img src="profile-icon.png" class="img-fluid" id="icon" alt="profile"></td><td>Profile</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class = "col-sm-7">
+                <img src="stories.png" class="img-fluid mx-auto d-block" alt="stories">
+                    <table class="table table-borderless mt-2">
+                        <tbody>
+                            <tr>
+                                <td><img src="post00.png" class="img-fluid mx-auto d-block" alt="post"></td>
+                            </tr>
+                            <tr>
+                                <td><video width="470" class="mx-auto d-block" alt="post" controls>
+                                    <source src="goddess-laufey.mp4" type="video/mp4">
+                                </video></td>
+                            </tr>
+                            <tr>                            
+                                <td><img src="post00-1.png" class="img-fluid mx-auto d-block" alt="post"></td>
+                            </tr>
+                            <tr>
+                                <td><img src="post01.png" class="img-fluid mx-auto d-block" alt="post"></td>
+                            </tr>
+                            <tr>
+                                <td><img src="post02.png" class="img-fluid mx-auto d-block" alt="post"></td>
+                            </tr>
+                            <tr>
+                                <td><img src="post03.png" class="img-fluid mx-auto d-block" alt="post"></td>
+                            </tr>
+                            <tr>
+                                <td><img src="post04.png" class="img-fluid mx-auto d-block" alt="post"></td>
+                            </tr>
+                            <tr>
+                                <td><img src="post05.png" class="img-fluid mx-auto d-block" alt="post"></td>
+                            </tr>
+                            <tr>
+                                <td><img src="post06.png" class="img-fluid mx-auto d-block" alt="post"></td>
+                            </tr>
+                            <tr>
+                                <td><video width="470" class="mx-auto d-block" alt="post" controls>
+                                    <source src="conyo.mp4" type="video/mp4">
+                                </video></td>
+                            </tr>
+                            <tr>                            
+                                <td><img src="post06-1.png" class="img-fluid mx-auto d-block" alt="post"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+            </div>
+            <div class = "col-sm-3">
+            <div class = "container me-2">
+                    <table class="table table-borderless">
+                        <tbody>
+                        <tr class="link-primary">
+                            <td><img src="pro01.png" class="img-fluid" id="profiles" alt="home"></td><td><small>Switch</small></td>
+                        </tr>
+                        <tr class="link-light">
+                            <td>Suggested for you</td><td><small>See All</small></td>
+                        </tr>
+                        <tr class="link-primary">
+                            <td><img src="pro02.png" class="img-fluid" id="profiles" alt="explore"></td><td><small>Follow</small></td>
+                        </tr>
+                        <tr class="link-primary">
+                            <td><img src="pro03.png" class="img-fluid" id="profiles" alt="reels"></td><td><small>Follow</small></td>
+                        </tr>
+                        <tr class="link-primary">
+                            <td><img src="pro04.png" class="img-fluid" id="profiles" alt="mess"></td><td><small>Follow</small></td>
+                        </tr>
+                        <tr class="link-primary">
+                            <td><img src="pro05.png" class="img-fluid" id="profiles" alt="notif"></td><td><small>Follow</small></td>
+                        </tr>
+                        <tr class="link-primary">
+                            <td><img src="pro06.png" class="img-fluid" id="profiles" alt="create"></td><td><small>Follow</small></td>
+                        </tr>
+                        <tr class = "link-secondary p-0">
+                            <td><p><small>About·Help·Press·API·Jobs·Privacy·Terms·</small><p>
+                                <p><small>Locations·Language·Meta Verifid</small></p>
+                                <p><small>© 2024 INSTAJ0o0LS FROM ME-TA-U</small></p>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
